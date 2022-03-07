bc5040582206d68027cae83a8f22aa2c9aa690f8
Michael S"exit()quit()git config --global user.name Michael <michael.sosu@amalitech.org>
2022-03-07 18:18:07 +0000

    Bug fixes

--- tourist_attractions/forms.py
+++ tourist_attractions/forms.py
@@ -3,10 +3,10 @@ from .models import State, Attraction
 
 class StateCreateForm(forms.ModelForm):
   class Meta:
-    model= Sate
+    model= State
     fields = '__all__'
 
-  class AttractionCreateForm(forms.ModelForm):
-    class Meta:
-      model = Attraction
-      fields = '__all__'
+class AttractionCreateForm(forms.ModelForm):
+  class Meta:
+    model = Attraction
+    fields = '__all__'
